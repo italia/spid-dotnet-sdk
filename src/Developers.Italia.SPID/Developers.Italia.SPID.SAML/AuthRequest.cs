2c4b93bfae43882c1abadc8ddccc04f3e3fd6755
Luca Congiu <congiuluc@gmail.com>
2017-10-11 14:46:36 +0200

    Commit Init

---
+++ src/Developers.Italia.SPID/Developers.Italia.SPID.SAML/AuthRequest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Developers.Italia.SPID.SAML
+{
+
+
+    public class AuthRequestOptions {
+
+    }
+
+    public class AuthRequest
+    {
+        public AuthRequestOptions _options { get; set; }
+
+        public AuthRequest(AuthRequestOptions options)
+        {
+            this._options = options;
+        }
+
+        public string GetAuthRequest()
+        {
+            string result="";
+
+            return result;
+
+        }
+
+        public string GetSignedAuthRequest() {
+
+            string result="";
+
+
+            return result;
+        }
+
+    }
+}
