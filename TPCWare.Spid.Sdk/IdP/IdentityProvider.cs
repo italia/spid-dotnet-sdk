a1e08661763d4086abdf01813635eaff20870b05
Nicolò Carandini <n.carandini@outlook.com>
2017-10-21 03:02:07 +0200

    Extract and collapse IdPs properties and methods into class and selector

---
+++ TPCWare.Spid.Sdk/IdP/IdentityProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPCWare.Spid.Sdk.IdP
+{
+    public class IdentityProvider
+    {
+
+        public string ProviderName { get; private set; }
+
+        public string SpidServiceUrl { get; private set; }
+
+        public Func<DateTime, string> Now;
+        public Func<DateTime, string> After;
+        public Func<DateTime, string> NotBefore;
+
+        public IdentityProvider(string providerName, string spidServiceUrl, Func<DateTime, string> now, Func<DateTime, string> after, Func<DateTime, string> notBefore)
+        {
+            ProviderName = providerName;
+            SpidServiceUrl = spidServiceUrl;
+            Now = now;
+            After = after;
+            NotBefore = notBefore;
+        }
+    }
+}
---
+++ TPCWare.Spid.Sdk/IdP/IdentityProviderSelector.cs
@@ -0,0 +1,78 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPCWare.Spid.Sdk.IdP
+{
+    public static class IdentityProviderSelector
+    {
+        private static ILog log = log4net.LogManager.GetLogger(typeof(IdentityProviderSelector));
+
+        public static IdentityProvider GetIdpFromUserChoice(string idpLabel, bool forTesting = false)
+        {
+            IdentityProvider idp;
+
+            if (string.IsNullOrWhiteSpace(idpLabel))
+            {
+                log.Error("Error on GetIdpFromUserChoice: The idpLabel parameter is null.");
+                throw new ArgumentNullException("The idpLabel parameter can't be null.");
+            }
+
+            switch (idpLabel)
+            {
+                case "poste_id":
+                    idp = new IdentityProvider(
+                        providerName: "Poste Italiane",
+                        spidServiceUrl: forTesting ? "https://spidposte.test.poste.it/jod-fs/ssoservicepost" : "",
+                        now: (now) => { return now.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'"); },
+                        after: (after) => { return after.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'"); },
+                        notBefore: (now) => { return now.AddMinutes(-2).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'"); }
+                    );
+                    break;
+
+                case "sielte_id":
+                    idp = new IdentityProvider(
+                        providerName: "Sielte",
+                        spidServiceUrl: forTesting ? "" : "",
+                        now: (now) => { return now.AddMinutes(-2).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"); },
+                        after: (after) => { return after.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"); },
+                        notBefore: (now) => { return now.AddMinutes(-2).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"); }
+                    );
+                    break;
+
+                case "tim_id":
+                    idp = new IdentityProvider(
+                        providerName: "TIM",
+                        spidServiceUrl: forTesting ? "" : "",
+                        now: (now) => { return now.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'"); },
+                        after: (after) => { return after.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'"); },
+                        notBefore: (now) => { return now.AddMinutes(-2).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'"); }
+                    );
+                    break;
+
+                case "infocert_id":
+                    idp = new IdentityProvider(
+                        providerName: "Infocert",
+                        spidServiceUrl: forTesting ? "" : "",
+                        now: (now) => { return now.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'"); },
+                        after: (after) => { return after.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'"); },
+                        notBefore: (now) => { return now.AddMinutes(-2).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'"); }
+                    );
+                    break;
+
+                default:
+                    log.Error($"Error on GetIdpFromUserChoice: Identity Provider not found for idpLabel = {idpLabel}.");
+                    throw new Exception($"Error on GetIdpFromUserChoice: Identity Provider not found for idpLabel = {idpLabel}.");
+            }
+
+            if (string.IsNullOrWhiteSpace(idp.SpidServiceUrl))
+            {
+                log.Error($"Error on GetIdpFromUserChoice: Identity Provider {idpLabel} doesn't have a {(forTesting ? "test" : "production")} endpoint.");
+                throw new Exception($"Error on GetIdpFromUserChoice: Identity Provider {idpLabel} doesn't have a {(forTesting ? "test" : "production")} endpoint.");
+            }
+
+            return idp;
+        }
+    }
+}
